aec4c39522bbc69fbc4cc9895ed3acd801eb5aac
jake <jakewdr@proton.me>
2024-10-31 14:19:08 +0000

    Reducing bundle size
    - Swapped out ts-particles for the basic version
    - Added vite config options
    - Some reformatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,37 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-    site: 'https://jakewdr.github.io'
+    site: 'https://jakewdr.github.io',
+    vite: {
+        optimizeDeps: {
+            include: [
+                "astro-particles", 
+                "tsparticles-engine", 
+                "tsparticles-basic"
+            ],
+            exclude: [
+                "@astrojs/check",
+                "@biomejs/biome",
+                "astro",
+                "typescript"
+            ]
+        },
+        build: {
+            cssCodeSplit: true,
+            minify: "terser",
+            terserOptions: {
+                compress: {
+                    unsafe: true,
+                    drop_console: true,
+                    arguments: true
+                },
+                mangle: {
+                    toplevel: true
+                },
+                format: {
+                    quote_style: 1,
+                }
+            }
+        },
+    }
 })
