1bbaa050afdb347e3b5e5e152399c56ce7c543b4
jake <jakewdr@proton.me>
2024-11-01 12:16:51 +0000

    Converted astro components to tsx ones

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import type React from "react";
+
+const Footer: React.FC = () => {
+    const footerStyle: React.CSSProperties = {
+        display: "flex",
+        gap: "1rem",
+        marginTop: "2rem",
+    };
+
+    const pStyle: React.CSSProperties = {
+        padding: "0.5rem 1rem",
+        color: "#000000",
+        backgroundColor: "#D3D3D3",
+        textDecoration: "none",
+    };
+
+    return (
+        <footer style={footerStyle}>
+            <p style={pStyle}>
+                <i>
+                    Written by me in <a href="https://astro.build/">Astro</a>
+                </i>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+
+const NavLinks: React.FC = () => {
+    return (
+        <div className="nav-links">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/links">Links</a>
+        </div>
+    );
+};
+
+export default NavLinks;
