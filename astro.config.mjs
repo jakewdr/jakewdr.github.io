e06351bb9fe03c69cd9e5e0b0a64cf0a8129b16b
jake <jakewdr@proton.me>
2024-11-06 12:54:59 +0000

    Revert "Switching to vercel for hosting"
    This reverts commit b2cc7b96d9fedf2d1908d648c39250099da7bc56.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 
 export default defineConfig({
+    site: "https://jakewdr.github.io",
 
     vite: {
         build: {
@@ -13,7 +12,4 @@ export default defineConfig({
             chunkSizeWarningLimit: 100,
         },
     },
-
-    output: "server",
-    adapter: vercel(),
 });
