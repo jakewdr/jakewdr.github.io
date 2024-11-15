538537a7b9f38b56bd156a26cea82c3b4978b186
jake <jakewdr@proton.me>
2024-11-15 14:02:27 +0000

    No base styles please

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,5 +22,5 @@ export default defineConfig({
         plugins: [visualizer()],
     },
 
-    integrations: [preact(), tailwind()],
+    integrations: [preact(), tailwind({applyBaseStyles: false})],
 });
