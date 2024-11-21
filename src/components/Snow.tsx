e5d67d39a943a7b325b10d24f00ab7c05952cbe5
jake <jakewdr@proton.me>
2024-11-21 15:38:12 +0000

    Realized I actually need suspense

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,22 +1,24 @@
-import { lazy } from "preact/compat";
+import { lazy, Suspense } from "preact/compat";
 
 const Snowfall = lazy(() => import("react-snowfall"));
 
 const Snow = () => {
     return (
-        <Snowfall
-            style={{
-                position: "fixed",
-                width: "100vw",
-                height: "100vh",
-            }}
-            color="#E2E8F0"
-            snowflakeCount={175}
-            wind={[-0.25, 1.5]}
-            speed={[0.5, 2]}
-            radius={[2, 3]}
-            changeFrequency={200}
-        />
+        <Suspense fallback={null}>
+            <Snowfall
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                }}
+                color="#E2E8F0"
+                snowflakeCount={175}
+                wind={[-0.25, 1.5]}
+                speed={[0.5, 2]}
+                radius={[2, 3]}
+                changeFrequency={200}
+            />
+        </Suspense>
     );
 };
 
