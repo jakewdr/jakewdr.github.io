ea1af74debf1207bdf819306710281772c26a874
jake <jakewdr@proton.me>
2024-11-21 13:00:00 +0000

    Stopped terser form collapsing inline tag whitespace

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
             HTML: {
                 "html-minifier-terser": {
                     removeAttributeQuotes: false,
+                    collapseInlineTagWhitespace: false
                 },
             },
         }),
