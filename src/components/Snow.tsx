ab17d2a8a81cc65420a94c59df09d3c66359519e
jake <jakewdr@proton.me>
2024-11-21 15:06:20 +0000

    Converted Snow.tsx to use an async import for snow

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,20 +1,24 @@
-import Snowfall from "react-snowfall";
+import { Suspense, lazy } from "preact/compat";
+
+const Snowfall = lazy(() => import("react-snowfall"));
 
 const Snow = () => {
     return (
-        <Snowfall
-            style={{
-                position: "fixed",
-                width: "100vw",
-                height: "100vh",
-            }}
-            color="#E2E8F0"
-            snowflakeCount={175}
-            wind={[-0.25, 1.5]}
-            speed={[0.5, 2]}
-            radius={[2, 3]}
-            changeFrequency={200}
-        />
+        <Suspense fallback={<div />}>
+            <Snowfall
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                }}
+                color="#E2E8F0"
+                snowflakeCount={175}
+                wind={[-0.25, 1.5]}
+                speed={[0.5, 2]}
+                radius={[2, 3]}
+                changeFrequency={200}
+            />
+        </Suspense>
     );
 };
 
