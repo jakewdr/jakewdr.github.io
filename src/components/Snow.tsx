3074dd646ed1ba1b729cbd2ab0755e9508bafef4
jake <jakewdr@proton.me>
2024-11-22 11:25:38 +0000

    Slight changes
    - Changed wind values
    - Added DNS prefetch to external links

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -13,7 +13,7 @@ const Snow = () => {
                 }}
                 color="#E2E8F0"
                 snowflakeCount={175}
-                wind={[-0.25, 1.5]}
+                wind={[-0.5, 0.5]}
                 speed={[0.5, 2]}
                 radius={[2, 3]}
                 changeFrequency={200}
