cbfa4a059bcd88a73ba41e0ea3f71fc1e35f150b
jake <jakewdr@proton.me>
2024-11-25 15:22:09 +0000

    Removed preact/compat
    - Prefer to use the lazy loading of astro client:idle instead
    - Reduced snow particle count to improve readability on mobile

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,24 +1,20 @@
-import { Suspense, lazy } from "preact/compat";
-
-const Snowfall = lazy(() => import("react-snowfall"));
+import Snowfall from "react-snowfall";
 
 const Snow = () => {
     return (
-        <Suspense fallback={null}>
-            <Snowfall
-                style={{
-                    position: "fixed",
-                    width: "100vw",
-                    height: "100vh",
-                }}
-                color="#E2E8F0"
-                snowflakeCount={175}
-                wind={[-0.5, 0.5]}
-                speed={[0.5, 2]}
-                radius={[2, 3]}
-                changeFrequency={200}
-            />
-        </Suspense>
+        <Snowfall
+            style={{
+                position: "fixed",
+                width: "100vw",
+                height: "100vh",
+            }}
+            color="#E2E8F0"
+            snowflakeCount={125}
+            wind={[-0.5, 0.5]}
+            speed={[0.5, 2]}
+            radius={[2, 3]}
+            changeFrequency={200}
+        />
     );
 };
 
